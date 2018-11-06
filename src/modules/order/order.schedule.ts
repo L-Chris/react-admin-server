399664c4efdb9524f9a4e8f924f979f1162a909a
L-Chris <563303226@qq.com>
2018-11-06 08:35:51 +0800

    update: 定时任务*钉钉

--- src/modules/order/order.schedule.ts
+++ src/modules/order/order.schedule.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { Injectable } from '@nestjs/common';
 import { Cron, NestSchedule } from 'nest-schedule';
 
@@ -7,10 +8,18 @@ export class OrderSchedule extends NestSchedule {
     super()
   }
 
-  @Cron('0 0 18/23 * * ?', {
+  @Cron('0 0 18 * * ?', {
     tz: 'Asia/Shanghai'
   })
-  notice () {
-    console.log(1)
+  async notice () {
+    await axios.post('https://oapi.dingtalk.com/robot/send?access_token=', {
+      msgtype: 'link',
+      link: {
+        text: '1. 打开链接，查看菜单；\n2. 选择店铺和菜品并提交',
+        title: '今天加班的童鞋记得点餐哈~',
+        picUrl: '',
+        messageUrl: 'http://www.baidu.com'
+      }
+    })
   }
 }
